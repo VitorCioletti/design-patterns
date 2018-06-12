eb39225ad1607df8501e974fc84709715e01b305
VitorCioletti <vitor.morais@aec.com.br>
2018-06-12 17:30:16 -0300

    Corrigido problema de configuração de .csproj

--- TrabalhoPadraoProjeto/Observer/Properties/AssemblyInfo.cs
+++ TrabalhoPadraoProjeto/Observer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TrabalhoPadraoProjeto")]
+[assembly: AssemblyTitle("Observer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TrabalhoPadraoProjeto")]
+[assembly: AssemblyProduct("Observer")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("40675604-0583-4716-807e-a15c84b397fa")]
+[assembly: Guid("812f57ac-0d54-4e11-9f23-c55768c0a3b8")]
 
 // Version information for an assembly consists of the following four values:
 //
